2c8a260c67184661f5c854295a3e6648f92c5e5c
Colin Wuest <Colin.Wuest@balluff.de>
2021-04-21 08:07:06 -0700

    amd64 build only

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ VERSION=1.18.0
 DOCKER_CLI_EXPERIMENTAL=enabled docker buildx create --name mybuilder
 DOCKER_CLI_EXPERIMENTAL=enabled docker buildx use mybuilder
 
-DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag distroless/distroless-nginx:$VERSION .
-DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag distroless/distroless-nginx:latest .
+DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --push --platform linux/amd64 --tag distroless/distroless-nginx:$VERSION .
+DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --push --platform linux/amd64 --tag distroless/distroless-nginx:latest .

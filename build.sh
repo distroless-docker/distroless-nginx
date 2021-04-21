509d8f03dadc1743647bcf6c10eec59f6dcc53fe
Colin Wuest <Colin.Wuest@balluff.de>
2021-04-21 07:45:27 -0700

    Added builder

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 VERSION=1.18.0
 
+DOCKER_CLI_EXPERIMENTAL=enabled docker buildx create --name mybuilder
+DOCKER_CLI_EXPERIMENTAL=enabled docker buildx use mybuilder
+
 DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag distroless/distroless-nginx:$VERSION .
 DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag distroless/distroless-nginx:latest .

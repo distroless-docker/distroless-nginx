00bd5d769826c66ceda80c39e8e11f25bd945434
colinwuest <82584805+colinwuest@users.noreply.github.com>
2021-04-21 16:23:56 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 VERSION=1.18.0
 
 DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKERHUB:$VERSION .
+DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKERHUB:latest .

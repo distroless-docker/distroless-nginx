fa5424718fc1f64a8ff1454b2d5070eaacc3e531
Seidel-Michael <michael.seidel@balluff.de>
2021-04-22 14:35:36 +0200

    ci: Fix Buildscript

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ docker run --rm --privileged docker/binfmt:820fdd95a9972a5308930a2bdfb8573dd4447
 DOCKER_CLI_EXPERIMENTAL=enabled docker buildx create --name mybuilder --use
 
 
-DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag distroless/distroless-nginx:$VERSION .
+DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag distrolessdocker/distroless-nginx:$VERSION .
